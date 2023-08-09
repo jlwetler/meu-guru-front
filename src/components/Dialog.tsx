4e1ae931cdfca826f947c9cd59334fa4703a3382
jlwetler <joaolucasmw@gmail.com>
2023-08-08 23:58:15 -0300

    refactor: dialog styled components

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from 'styled-components';
 
 interface DialogProps {
   open: boolean;
@@ -7,37 +8,41 @@ interface DialogProps {
 }
 
 const Dialog = ({open, onClose, children}: DialogProps) => {
-  return (<div style={{
-    zIndex: 9999,
-    position: "fixed",
-    left: 0,
-    top: 0,
-    width: "100vw",
-    height: "100vh",
-    display: open ? "flex" : "none",
-    alignItems: "center",
-    justifyContent: "center",
-    background: "gray",
-    opacity: "96%"
-  }}>
-    <div style={{
-      border: "1px solid #000",
-      width: "400px",
-      height: "500px",
-      display: open ? "block" : "none",
-      background: "white",
-      padding: "20px",
-      borderRadius: "40px"
-    }}>
-      <div style={{
-        fontSize: "20px",
-        cursor: "pointer",
-        float: "right",
-      }}
-      onClick={onClose}> X </div>
+  return (
+  <Background style={{display: open ? "flex" : "none"}}>
+    <DialogBox style={{ display: open ? "block" : "none"}}>
+      <CloseButton onClick={onClose}> X </CloseButton>
       {children}
-    </div>
-  </div>);
+    </DialogBox>
+  </Background>);
 }
+
+const Background = styled.div`
+  z-index: 999;
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100vw;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+  background: gray;
+  opacity: 96%;
+`;
+
+const DialogBox = styled.div`
+  border: 1px solid #000;
+  width: 400px;
+  height: 500px;
+  background: white;
+  padding: 20px;
+  border-radius: 40px;
+`;
+
+const CloseButton = styled.div`
+  font-size: 20px;
+  cursor: pointer;
+  float: right;
+`;
 
 export default Dialog;
